cfdd322aa22915cc878ff55f770f03515190133f
zulu-M <zhtet7864@gmail.com>
2024-12-18 01:29:40 +0700

    304 MVC architectured finished raw

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+// 'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+
+export const TIMEOUT_SEC = 10;
---
+++ src/js/model.js
@@ -0,0 +1,26 @@
+import { API_URL } from './config.js';
+import { getJson } from './helper.js';
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJson(`${API_URL}/${id}`);
+    console.log(data);
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    console.error(`${err} 🔥🔥🔥🔥`);
+  }
+};
